038b7a1c7f6657d904573b88019d53e64020a3c9
benwright <benwright6969@gmail.com>
2019-02-13 16:15:32 +0000

    Testing js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 for (var i = 0; i <= 20; i++){
   if (i % 2 === 0){
-    document.write(i + ' is even ');
-    document.write("<br>");
+    console.log(i + ' is even ');
+  //  document.write("<br>");
   } else {
-   document.write(i + ' is odd' +"\n");
-   document.write("<br>");
+   console.log(i + ' is odd' +"\n");
+  // document.write("<br>");
   }
 }
