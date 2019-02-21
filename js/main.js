904d1aec7bd820a68546c22a1f01e0acd0954a55
benwright <benwright6969@gmail.com>
2019-02-21 15:31:47 +0000

    Cleaned up code and added random lyrics to see if it looks better with more text

--- js/main.js
+++
@@ -1,9 +0,0 @@
-for (var i = 0; i <= 20; i++){
-  if (i % 2 === 0){
-    console.log(i + ' is even ');
-  //  document.write("<br>");
-  } else {
-   console.log(i + ' is odd' +"\n");
-  // document.write("<br>");
-  }
-}
