eea8a12d1c2198a30a1dfe0d0710b8ff4d2101b8
benwright <benwright6969@gmail.com>
2020-08-24 22:53:10 +0100

    Added preloading for images

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Preload Images
+var images = new Array();
+
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
+}
+
+preloadImages("https://i.imgur.com/LLLhaCb.png", "https://i.imgur.com/izA4bqy.png", "https://i.imgur.com/V2slDHT.png");
+// End Preload Images
